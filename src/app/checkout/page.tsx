fc20eb1fda30d3d625dbe7393d3fed82295651cd
DEVA Gaikwad <devanandgaikwad2@gmail.com>
2025-08-25 13:47:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
